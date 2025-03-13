7074a5203a037ae0bc4666bb8f5a94c602b0de02
Vatsal Srivatsava <109857475+09Vatsal@users.noreply.github.com>
2025-03-13 09:42:34 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+const firebaseConfig = {
+ apiKey: "Your_api-key",
+  authDomain: "rYour_auth_domain",
+  projectId: "Your_project_ID",
+  storageBucket: "Your_storage_bucket",
+  messagingSenderId: "Your_messaging_sender_id",
+  appId: "Your_app_id",
+  measurementId: "Your_measurment_id"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage, collection, addDoc, query, orderBy, onSnapshot, ref, uploadBytes, getDownloadURL };
